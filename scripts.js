b6881cc28bb05bae6b14a43e904df0416f524db1
Pablo Satler <pablosatler@gmail.com>
2021-02-05 09:48:59 -0300

    feat: add transaction via js
    - select table body
    - create table rows and append them to the table body
    - format the values according to the currency pt-BR

--- scripts.js
+++ scripts.js
@@ -1,14 +1,88 @@
 const Modal = {
-  open() {
+  toggle() {
     document
       .querySelector('.modal-overlay')
       .classList
-      .add('active')
+      .toggle('active')
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000, // not using commas to split the cents
+    date: '30/01/2021',
   },
-  close() {
-    document
-      .querySelector('.modal-overlay')
-      .classList
-      .remove('active')
+  {
+    id: 2,
+    description: 'Criação website',
+    amount: 500000, // not using commas to split the cents
+    date: '30/01/2021',
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: 20000, // not using commas to split the cents
+    date: '30/01/2021',
+  },
+]
+
+const Transaction = {
+  income() {
+
+  },
+  expenses() {
+
+  },
+  total() {
+
   },
 }
+
+const DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr')
+    tr.innerHTML = this.innerHTMLTransaction(transaction)
+
+    this.transactionsContainer.appendChild(tr)
+  },
+  innerHTMLTransaction(transaction) {
+    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+
+    const amount = Utils.formatCurrency(transaction.amount)
+
+    const html = `
+      <td class="description">${transaction.description}</td>
+      <td class="${CSSclass}">${amount}</td>
+      <td class="date">${transaction.date}</td>
+      <td>
+        <img src="./assets/plus.svg" alt="Adicionar transação">
+      </td>
+    `
+
+    return html
+  }
+}
+
+
+const Utils = {
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? '-' : ''
+
+    value = String(value).replace(/\D/g, "") // replacing all non number chars by empty string
+    value = Number(value) / 100
+    value = value.toLocaleString("pt-BR", {
+      style: 'currency',
+      currency: 'BRL'
+    })
+
+    return signal + value
+  }
+}
+
+transactions.forEach(function(transaction) {
+  DOM.addTransaction(transaction)
+})
